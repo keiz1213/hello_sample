39e9c44b65f8d44ca4c083337e0b3b0c1a2d2a6b
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-01 01:18:32 +0900

    rake

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_150438) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_153055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_150438) do
     t.string "avatar_url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["uid"], name: "index_users_on_uid"
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
 end
