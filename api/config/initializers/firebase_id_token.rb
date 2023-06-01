b3b7e0a2a02e5325694e2c9a79a5654fd5b972ef
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-01 18:22:14 +0900

    redis

--- api/config/initializers/firebase_id_token.rb
+++ api/config/initializers/firebase_id_token.rb
@@ -1,4 +1,4 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(host: 'redis')
+  config.redis = Redis.new(host: ENV['REDIS_HOST'])
   config.project_ids = [ENV['FIREBASE_PROJECT_ID']]
 end
