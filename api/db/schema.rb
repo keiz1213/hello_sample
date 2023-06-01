b6f8fba2d4b9626aa582ffa7d8293bbeef92ef8c
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-01 17:45:34 +0900

    add hello

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_082936) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_084333) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_082936) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_games_on_user_id", unique: true
+  end
+
+  create_table "hellos", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "quizzes", force: :cascade do |t|
