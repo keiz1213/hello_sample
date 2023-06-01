f7d89858b19581feb7bf811f93784618442a2e11
keiz1213 <fukuoka00tsc@gmail.com>
2023-06-02 01:40:49 +0900

    test

--- api/config/initializers/firebase_id_token.rb
+++ api/config/initializers/firebase_id_token.rb
@@ -1,5 +1,5 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(host: 'fly-hello-sample-redis.upstash.io', port: 36677, password: '36e7bded575e49c3b126e56339fa13a8')
-  # config.redis = Redis.new(host: ENV['REDIS_URL'], port: 6379)
+  # config.redis = Redis.new(host: 'fly-hello-sample-redis.upstash.io', port: 36677, password: '36e7bded575e49c3b126e56339fa13a8')
+  config.redis = Redis.new(url: 'redis://default:11254a05625f4cdbae6bf770c75b76d0@fly-hello-sample-redis.upstash.io')
   config.project_ids = [ENV['FIREBASE_PROJECT_ID']]
 end
